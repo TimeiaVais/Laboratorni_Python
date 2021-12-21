0511b3c7df8d8514480d848897b8a484ca458e71
vaistimeia4@gmail.com <0991810366t>
2021-12-21 22:34:09 +0200

    add lab_8_4

--- Labka_8/labka_8_4.py
+++ Labka_8/labka_8_4.py
@@ -1,10 +1,20 @@
 #Розмістити елементи  діагоналі матриці у порядку спадання.
-a = [#0 1  2
-    [1, 2, 3], #0
-    [6, 8, 1], #1
-    [5, 7, 9]  #2
+def sortDiagonal(a,M,N):
+    for i in range(M):
+        sm = a[i][i]
+        pos = i
+        for j in range(i+1,N):
+            if (sm < a[j][j]):
+                sm = a[j][j]
+                pos = j
+        a[i][i], a[pos][pos] = a[pos][pos], a[i][i]
+    for i in range(M):
+        for j in range(N):
+            print(a[i][j],end=" ")
+        print()
+a = [
+    [4,2,1],
+    [3,1,5],
+    [8,9,12]
 ]
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        a.sort(reverse = True)
-print(*a, sep = '\n')
+sortDiagonal(a,3,3)
